a06c06f49b2c8c9adab6bda0ff191c9fb7971a58
Aldo Fadilla Putra <aldo15ti@mahasiswa.pcr.ac.id>
2020-01-08 11:53:31 +0700

    Updates

---
+++ precache-manifest.d720c2434e5bfc80a7baa9a24ee818f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32013d20c61fd5bca37da900f8289dc",
+    "url": "/recipes-search-app/index.html"
+  },
+  {
+    "revision": "f706c8b8e54b108b4ffa",
+    "url": "/recipes-search-app/static/css/main.44dc21cb.chunk.css"
+  },
+  {
+    "revision": "1e19189615b56bb9fceb",
+    "url": "/recipes-search-app/static/js/2.718419cc.chunk.js"
+  },
+  {
+    "revision": "f706c8b8e54b108b4ffa",
+    "url": "/recipes-search-app/static/js/main.9289b2c0.chunk.js"
+  },
+  {
+    "revision": "37defe4c493a57decd0a",
+    "url": "/recipes-search-app/static/js/runtime~main.1ccbf3d3.js"
+  }
+]);
